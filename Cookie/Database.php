52395b68da857f001d19f71ca95a8f774ec31f74
D3D53C <marc.steinebrunner@gmail.com>
2020-06-08 20:49:41 +0200

    Create Database.php

---
+++ Cookie/Database.php
@@ -0,0 +1,18 @@
+<?
+function Connect($host = localhost,$user = "root",$pass = "",$DB = "demo"){
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+  $link = mysqli_connect($host, $user, $pass, $DB);
+
+  if($link === false){
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+  }
+}
+
+function Insert($Value, Muster = [IP, Country, City, Time, Page]){
+  $sql = "INSERT INTO persons ($Muster) VALUES ($Value)";
+  echo $sql;
+}
+
+Insert(["192,168.2.1", "DE", "LOE", "12:00", "Main"]);
+?>
