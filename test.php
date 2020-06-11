edddf7d7ea155d42ec9f6e475587d8b106b77e31
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-11 19:26:52 +0200

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require "core\autoload.core.php";
     require "core\initVerwalung.core.php";
     global $userVW ;
-
+?>
 	<h1> User </h1>
 		
   <form acticon="test.php" method="POST">
@@ -13,6 +13,7 @@ require "core\autoload.core.php";
 		
     <button type="submit" name="action" value="0">Submit</button>
   </form>
+<?
 		var_dump($_POST)
   $userVW->adduser($_POST['username'], $_POST['role'], $_POST['email'], password_hash($_POST['password']))
 ?>
