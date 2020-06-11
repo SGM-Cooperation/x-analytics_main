b95bead4c62aac819e51e2581e11c2fb80f12ea6
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-11 19:41:13 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
     if(isset($_POST['SubmitButton'])){ // Check if form was submitted
+        $_POST['password'] = password_hash($_POST['password']);
         $message = var_dump($_POST);
     }
 ?>
