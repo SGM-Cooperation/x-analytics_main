e685ed2accfe557c2ac1c179f0dad175fa2978aa
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-11 19:23:01 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,17 @@
 <?
 require "core\autoload.core.php";
     require "core\initVerwalung.core.php";
-    global $productVW;
+    global $nutzerVW;
+
 	<h1> User </h1>
+		
   <form acticon="test.php" method="POST">
     <input type="text" name="username">
     <input type="number" name="role">
     <input type="email" name="email">
     <input type="password" name=password>
+		
+    <button type="submit" name="action" value="0">Submit</button>
   </form>
   $nutzerVW->adduser($_POST['username'], $_POST['role'], $_POST['email'], password_hash($_POST['password']))
 ?>
