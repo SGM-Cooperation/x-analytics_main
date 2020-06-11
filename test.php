884dd1709bb22a4c86fd6e1163a84b5a00cdb2df
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-11 19:30:15 +0200

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require "core\autoload.core.php";
 ?>
 	<h1> User </h1>
 		
-  <form acticon="test.php" method="POST">
+  <form acticon="/test.php" method="post">
     <input type="text" name="username">
     <input type="number" name="role">
     <input type="email" name="email">
@@ -14,6 +14,6 @@ require "core\autoload.core.php";
     <button type="submit" name="action" value="0">Submit</button>
   </form>
 <?
-		var_dump($_POST)
+  var_dump($_POST)
   $userVW->adduser($_POST['username'], $_POST['role'], $_POST['email'], password_hash($_POST['password']))
 ?>
