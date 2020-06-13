7bc9c30ec38adda2920dbeeb3f33f12309e7be25
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-13 23:21:55 +0200

    Update test.php

--- test.php
+++ test.php
@@ -2,8 +2,7 @@
 #require "core\autoload.core.php";
 #require "..\core\initVerwalung.core.php";
 $dir    = $_SERVER['DOCUMENT_ROOT'];
-echo $dir . "\core";
-#$files1 = scandir("$dir\core");
+$files1 = scandir($dir . "\core");
 
     if(isset($_POST['SubmitButton'])){ // Check if form was submitted
        #$userVW->adduser($username, $role, $email, $hashedpw);
