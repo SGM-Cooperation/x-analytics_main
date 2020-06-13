81a7cef97379d40f0feeb24a735e5d72577712dc
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-13 23:30:21 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
-$dir    = $_SERVER['DOCUMENT_ROOT'];
-require "$dir . "/core/autoload.core.php"";
-require "$dir . "/core/initVerwalung.core.php"";
+require "/core/autoload.core.php";
+require "/core/initVerwalung.core.php";
 
 
     if(isset($_POST['SubmitButton'])){ // Check if form was submitted
