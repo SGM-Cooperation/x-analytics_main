8410491ac1d87307a7d726d15f83bfd41f472837
SGMCooperation <66268882+SGMCooperation@users.noreply.github.com>
2020-06-13 23:39:56 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,16 +1,5 @@
 <?php
 
-spl_autoload_register("autoloader");
-function autoloader($className)
-{
-  $path = "classes/";
-  $extension = ".class.php";
-  $fullPath = $path . $className . $extension;
-  if(!file_exists($fullPath)){
-    return false;
-  }
-  include_once $fullPath;
-}
 
 if(isset($userVW)){
   if (!($userVW instanceof Nutzerverwaltung)){
